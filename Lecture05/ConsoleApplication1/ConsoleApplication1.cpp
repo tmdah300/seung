84625de77e1f42994905ac6403e00aa05e4a6bf0
seungmo <tmdah400@gmail.com>
2023-04-03 19:54:23 +0900

    프로그래핑패턴5주차

---
+++ Lecture05/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <conio.h>
+
+int print_titleScreen()
+{
+	printf("***************************************\n");
+	printf("*                                     *\n");
+	printf("*                                     *\n");
+	printf("*             지렁이 게임             *\n");
+	printf("*            (Snake  Bite)            *\n");
+	printf("*                                     *\n");
+	printf("*    1. 게임 시작                     *\n");
+	printf("*    2. 게임 설명                     *\n");
+	printf("*    3. 게임 랭킹 보기                *\n");
+	printf("*    4. 게임 종료 (esc)               *\n");
+	printf("***************************************\n");
+
+	return 0;
+
+}
+
+int print_introductionScreen()
+{
+	printf("\n************************************\n");
+	printf("*       게임 설명 화면입니다.      *\n");
+	printf("************************************\n");
+	printf("타이틀 화면으로 돌아갈까요?(Y/N).\n\n");
+
+	return 0;
+}
+
+int print_playScreen()
+{
+	printf("********************************************\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("********************************************\n");
+	printf("타이틀 화면으로 돌아갈까요? (Y/N).\n\n");
+	return 0;
+}
+
+int main()
+{
+
+	char input;
+	int game_status=0, running = 1;
+
+	while (running)
+	{
+		switch (game_status)
+		{
+			case 0: print_titleScreen(); 
+					while (1)
+					{
+						input = _getch();
+						if (input == '1') {
+							game_status = 1; break;
+						}
+						if (input == '2') {
+							game_status = 2; break;
+						}
+						if (input == '4'||input == 27)
+						{
+							running = 0; break;
+						}
+						else
+						{
+							continue;
+						}
+					}
+					break;
+			case 1: print_playScreen();
+					while (true)
+					{
+						input = _getch();
+						if (input == 'y') {
+							game_status = 0; break;
+						}
+						else if (input == 'n') continue;
+					}
+				break;
+			case 2: print_introductionScreen(); 
+					while (1)
+					{
+						input = _getch();
+						if (input == 'y') {
+							game_status = 0; break;
+						}
+						else if (input == 'n') continue;
+					}
+					break;
+			default: break;
+		}
+
+		
+
+	}
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
---
+++ Lecture05/ConsoleApplication1/소스.cpp
@@ -0,0 +1,115 @@
+//#define DEBUG
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+// game_state == 0 일때
+int print_title_screen()
+{
+    std::cout << "******************************************" << std::endl;
+    std::cout << "*                                        *" << std::endl;
+    std::cout << "*                                        *" << std::endl;
+    std::cout << "*              지렁이 게임               *" << std::endl;
+    std::cout << "*             (Snake  Bite)              *" << std::endl;
+
+#ifdef DEBUG
+    std::cout << "*            - 디버그 모드 -             *" << std::endl;
+#else
+    std::cout << "*                                        *" << std::endl;
+#endif  
+    std::cout << "*   1. 게임 시작                         *" << std::endl;
+    std::cout << "*   2. 게임 설명                         *" << std::endl;
+    std::cout << "*   3. 게임 랭킹 보기                    *" << std::endl;
+    std::cout << "*   4. 게임 종료 (esc)                   *" << std::endl;
+    std::cout << "******************************************" << std::endl;
+
+    return 0;
+}
+
+void gotoxy(int x, int y) {
+    //x, y 좌표 설정
+    COORD pos = { x,y };
+    //커서 이동
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int print_game_screen(int stage_width, int stage_height)
+{
+    gotoxy(5, 5);
+
+    std::cout << "^^";
+
+
+
+    return 0;
+}
+
+// game_state == 2 일때
+int print_introduction_screen()
+{
+    std::cout << "******************************************" << std::endl;
+    std::cout << "타이틀화면으로 돌아갈까요? (Y/N)" << std::endl;
+    return 0;
+}
+
+int main()
+{
+
+    int game_state = 0;
+    int is_game_running = 1;
+
+    while (is_game_running)
+    {
+        char key_input = '0';
+        switch (game_state)
+        {
+        case 0:
+            print_title_screen();
+            key_input = _getch();
+            switch (key_input)
+            {
+            case '1':
+                game_state = 1;
+                break;
+            case '2':
+                game_state = 2;
+                break;
+            case '3':
+                break;
+            case '4':
+                is_game_running = 0;
+                break;
+            case 27:
+                is_game_running = 0;
+                break;
+            default:
+                break;
+            }
+            break;
+        case 1:
+            print_game_screen(10, 10);
+            key_input = _getch();
+        case 2:
+            print_introduction_screen();
+            key_input = _getch();
+            switch (key_input)
+            {
+            case 'y':
+                game_state = 0;
+                break;
+            case 'n':
+                break;
+            default:
+                break;
+            }
+            break;
+
+        default:
+            break;
+        }
+
+
+    }
+
+    return 0;
+}
